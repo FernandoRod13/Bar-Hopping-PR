7e9a7fb5e88ae3ceb00ad513a4e3be2324c6536a
Fernando Rodriguez <fernando.rodriguez10@upr.edu>
2017-11-26 01:10:20 -0400

    configureing firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+import { AngularFireModule } from 'angularfire2';
+import { environment } from '../environments/environment';
 import { AppComponent } from './app.component';
 
 
@@ -10,7 +10,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AngularFireModule.initializeApp(environment.firebase)
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyAA8nITd-sbZEqZjTrB2buE00x4sXFbyWg",
+    authDomain: "bar-hopping-55408.firebaseapp.com",
+    databaseURL: "https://bar-hopping-55408.firebaseio.com",
+    projectId: "bar-hopping-55408",
+    storageBucket: "bar-hopping-55408.appspot.com",
+    messagingSenderId: "342459890336"
+  }
 };
