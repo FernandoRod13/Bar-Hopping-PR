fad8c3c291d9966bd6533f906ca7fbc9941afab7
Fernando Rodriguez <fernando.rodriguez10@upr.edu>
2017-11-27 00:37:48 -0400

    started employee repo implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { EmployeesRepoService } from './repositories/employees/employees-repo.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app';
+  constructor(private employeeService: EmployeesRepoService){}
+  ngOnInit() {
+    this.employeeService.findEmployeesByPhoneNumber('7876244083').subscribe(employees => {
+      console.log(employees);
+    });
+  }
 }
