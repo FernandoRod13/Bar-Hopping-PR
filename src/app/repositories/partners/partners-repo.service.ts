b8b170e8fafa68617bca0e36b4893fa17c491660
Fernando Rodriguez <fernando.rodriguez10@upr.edu>
2017-11-26 20:37:27 -0400

    added definition of partner repository service

--- src/app/repositories/partners/partners-repo.service.ts
+++ src/app/repositories/partners/partners-repo.service.ts
@@ -1,8 +1,33 @@
 import { Injectable } from '@angular/core';
-
+import { AngularFirestore } from 'angularfire2/firestore';
+import { Observable } from 'rxjs/Observable';
 @Injectable()
 export class PartnersRepoService {
 
-  constructor() { }
+  constructor(db: AngularFirestore) { }
+  /**This function will add a new partner to the database. */
+  addNewPartner(partnerData: any): void {
+
+  }
+  /**This function will edit information in the database related to a specific partner. */
+  editPartnerInfo(partnerID: string, partnerData: any): void {
+
+  }
+  /**This function will add editing right to a user for a specific partner. */
+  addPartnerEditor(partnerID: string, userID: string): void {
+
+  }
+  /**This function will remove a partner form the database. */
+  removePartner(partnerID: string): void {
+
+  }
+  /**This function will return an observable collection of all our partners. */
+  getAllPartners(): Observable<any> {
+    return null;
+  }
+  /**This function will return an observable object of a specific partner. */
+  getSpecificPartner(partnerID: string): Observable<any> {
+    return null;
+  }
 
 }
