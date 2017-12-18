edcce8f7ccd9220c788a26005420f8250e4b8fed
Fernando Rodriguez <fernando.rodriguez10@upr.edu>
2017-12-18 01:57:37 -0400

    adding implementation of transaction repository

--- src/app/repositories/transactions/transactions-repo.service.ts
+++ src/app/repositories/transactions/transactions-repo.service.ts
@@ -1,21 +1,34 @@
 import { Injectable } from '@angular/core';
-import { AngularFirestore } from 'angularfire2/firestore';
+import { AngularFirestore, AngularFirestoreCollection } from 'angularfire2/firestore';
 import { Observable } from 'rxjs/Observable';
+import { Transaction } from './../../domainLayer/structures/Transaction';
+import { TransactionFactory } from './../../domainLayer/factories/transactionFactory';
 @Injectable()
 
 export class TransactionsRepoService {
 
-  constructor(db: AngularFirestore) { }
+  private transactionCollection: AngularFirestoreCollection<Transaction>;
+  constructor(private db: AngularFirestore, private factory: TransactionFactory) {
+    this.transactionCollection = this.db.collection('transactions');
+   }
   /**This function will add a new transaction to the database if transaction was successful. */
-  addNewTransaction(transactionDetails: any): void {
-
+  addNewTransaction(transactionDetails: Transaction): void {
+    this.transactionCollection.add(transactionDetails.exportData());
   }
   /**This fucntion will query for all transactions registered to a specific user int the database. */
-  getUserTransactions(userID: string): Observable<any> {
-    return null;
+  getUserTransactions(userID: string): Observable<Transaction[]> {
+    return this.db.collection('transactions', ref => ref.where('customerId', '==', userID)).snapshotChanges().map( item => {
+      return item.map( data => {
+        return this.factory.createTransaction(data.payload.doc);
+      });
+    });
   }
   /**This function will query for all transactions in the database. */
-  getStatement(): Observable<any> {
-    return null;
+  getStatement(): Observable<Transaction[]> {
+    return this.transactionCollection.snapshotChanges().map( item => {
+      return item.map( data => {
+        return this.factory.createTransaction(data.payload.doc);
+      });
+    });
   }
 }
