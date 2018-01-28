73106f9216fe5f1f3b850e6799bdf4f56eb2f822
FernandoRod13 <fernando.rodriguez10@upr.edu>
2018-01-28 01:41:36 -0400

    fixing get user

---
+++ src/app/domainLayer/structures/user.ts
@@ -0,0 +1,8 @@
+export class User {
+    constructor(
+        public uid: string,
+        public email: string,
+        public firstName: string,
+        public lastName: string
+    ) {}
+}
--- src/app/ui-layer/dashboards/account/account.component.ts
+++ src/app/ui-layer/dashboards/account/account.component.ts
@@ -1,20 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { AuthenticationService } from './../../../domainLayer/services/authentication/authentication.service';
-
+import { User } from './../../../domainLayer/structures/user';
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
   styleUrls: ['./account.component.css']
 })
-export class AccountComponent implements OnInit {
-  user: any;
+export class AccountComponent implements OnInit, OnDestroy {
+  user: User;
+  private userObserver;
   constructor( private auth: AuthenticationService) { }
 
   ngOnInit() {
-    this.auth.getUserInfo().subscribe( data => {
+    this.userObserver = this.auth.getUserInfo().subscribe( data => {
       this.user = data;
     });
 
+  }
+
+  ngOnDestroy() {
+    this.userObserver.unsubscribe();
   }
   public logout() {
     this.auth.logout();
